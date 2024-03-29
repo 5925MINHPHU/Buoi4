ce8308f4d0cdfa069164c84c357fb5f3971d3a1d
Administrator <Administrator@10PC323>
2024-03-29 15:39:53 +0700

    Add project files.

---
+++ QLBH/Models/EFCategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QLBH.Models
+{
+    public class EFCategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public EFCategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var product = await _context.Categories.FindAsync(id);
+            _context.Categories.Remove(product);
+            await _context.SaveChangesAsync();
+
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public Task<Category> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
